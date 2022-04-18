9315668d1e1c89d4ff01e5b07c3bcd1669949685
lola-source <adagaladolores@gmail.com>
2022-04-18 11:52:32 -0700

    first

---
+++ print_rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+int print_R(va_list R)
+{
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(R, char *);
+	if (str == NULL)
+		str = "(ahyy)";
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				_putchar(out[j]);
+				count++;
+				break;
+			}
+			if (!in[j])
+			{
+				_putchar(str[i]);
+				count++;
+			}
+		}
+		return (count);
+	}
+
